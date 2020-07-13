730f8fdd0b280857d884208c60e914d93aed9a13
Tzahi-14 <tzahidavid014@gmail.com>
2020-07-13 12:19:26 +0300

    4

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const { response } = require('express');
 
 
 
+
 app.get('/sanity', function (request, response) {
     response.send("OK")
 })
@@ -13,14 +14,21 @@ app.get('/recipes/:ingredient', function (request, response) {
     let ingredient = request.params.ingredient
     console.log(ingredient)
 
-})
-
-    urllib.request('https://recipes-goodness.herokuapp.com/recipes/YOUR_INGREDIENT', function (err, data, res) {
+    urllib.request(`https://recipes-goodness.herokuapp.com/recipes/${ingredient}`, function (err, data, res) {
         if (err) {
             throw err;
         }
+        const racipesArr = JSON.parse(data).results
+        const recipesList = []
+        for(let racipe of racipesArr){
+            const racipeInfo = {
+                ingredients :racipe.ingredients
+            }
+            recipesList.push(racipeInfo)
+        }
+        response.send(recipesList)
     })
-
+})
 
 app.use(express.static(path.join(__dirname, 'dist')))
 app.use(express.static(path.join(__dirname, 'node_modules')))
